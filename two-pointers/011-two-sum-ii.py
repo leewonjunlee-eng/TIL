fdcbbe26cf515f0cc26cca5ede292a1d169731f4
Leewonjun <leewonjunlee@gmail.com>
2026-07-23 15:08:29 +0900

    Implement twoSum method using two-pointer technique

---
+++ two-pointers/011-two-sum-ii.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            total = numbers[left] + numbers[right]
+
+            if total == target:
+                return [left + 1, right + 1]
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
+
+        return []
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    assert solution.twoSum([2, 7, 11, 15], 9) == [1, 2]
+    assert solution.twoSum([2, 3, 4], 6) == [1, 3]
+    assert solution.twoSum([-1, 0], -1) == [1, 2]
