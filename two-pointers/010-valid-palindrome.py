3000d1bac487de9d7510f5d39ea37a9be5131f60
Leewonjun <leewonjunlee@gmail.com>
2026-07-23 14:50:57 +0900

    Optimize problem 010 with two pointers

--- two-pointers/010-valid-palindrome.py
+++ two-pointers/010-valid-palindrome.py
@@ -1,16 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        forward = []
-        backward = []
-        filtered = "".join(char.lower() for char in s if char.isalnum())
+        left = 0
+        right = len(s) - 1
 
-        for char in filtered:
-            forward.append(char)
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
 
-        for char in reversed(filtered):
-            backward.append(char)
+            while left < right and not s[right].isalnum():
+                right -= 1
 
-        return forward == backward
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
 
 
 if __name__ == "__main__":
